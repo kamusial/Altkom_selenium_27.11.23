523b6549a6fd077067de69eec5b58d36a2ff520e
unknown <you@example.com>
2023-11-27 15:53:23 +0100

    1

--- OOP2.py
+++ OOP2.py
@@ -3,12 +3,28 @@ class Auto:
         self.kolor = barwa
         self.ilosc_paliwa = 10
         self.kondycja = stan
-        self.tryb_ekonomicnzy = False
+        self.tryb_ekonomiczny = False
         self.spalanie_na_100 = 14
         self.rocznik = 2023 - wiek
 
+    def zasieg(self):
+        zasieg = self.ilosc_paliwa / self.spalanie_na_100 * 100 * 0.9
+        return round(zasieg)
+
+    def ustaw_tryb(self, tryb):
+        if tryb == 'eco':
+            self.tryb_ekonomiczny = True
+            self.spalanie_na_100 = 10
+        elif tryb == 'normal':
+            self.tryb_ekonomiczny = False
+            self.spalanie_na_100 = 14
+        else:
+            print('bez zmian')
 
 
 
 moje_auto = Auto('black', 3, 12)
 print(moje_auto.kolor)
+
+print(moje_auto.spalanie_na_100)
+print(moje_auto.zasieg())
