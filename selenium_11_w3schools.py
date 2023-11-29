948d36f877f909fd8f0a44c0e7fe31623d7e1ef8
unknown <you@example.com>
2023-11-29 12:28:30 +0100

    1

--- selenium_11_w3schools.py
+++ selenium_11_w3schools.py
@@ -14,8 +14,33 @@ time.sleep(1)
 # menu.click()
 # time.sleep(2)
 menu = driver.find_element('id', 'navbtn_tutorials')
+# menu.click()
 HTMLtutorial = driver.find_element(By.XPATH, '//*[@id="tutorials_html_css_links_list"]/div[1]/a[1]')
+# HTMLtutorial.click()
+webdriver.ActionChains(driver).move_to_element(menu).click().move_to_element(HTMLtutorial).click().perform()
+HTML_forms_attributes = driver.find_element(By.XPATH, '//*[@id="leftmenuinnerinner"]/a[41]')
+HTML_forms_attributes.click()
+tryityourself = driver.find_element(By.XPATH, '//*[@id="main"]/div[3]/a')
+tryityourself.click()
+time.sleep(1)
 
+# runbtn = driver.find_element(By.ID, 'runbtn')
+# runbtn.click()
 
+currentWindowName = driver.current_window_handle
+windowNames = driver.window_handles
+print(currentWindowName)
+print(windowNames)
+
+for okno in windowNames:
+    if okno != currentWindowName:
+        driver.switch_to.window(okno)
+
+windowAdresses = []
+for karta in windowNames:
+    driver.switch_to.window(karta)
+    windowAdresses.append([driver.title, karta])
+
+print(windowAdresses)
 
 driver.quit()
